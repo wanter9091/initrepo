5f1ce787b1130dd274329dbe84edc1476cb238ba
wanter9091 <dhqudwo2976@naver.com>
2025-07-07 16:54:56 +0900

    코드 수정

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 코드 수정
         </a>
       </header>
     </div>
