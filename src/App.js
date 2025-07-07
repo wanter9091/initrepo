0a97140535c11da176928b55b24b3caebc256f57
wanter9091 <dhqudwo2976@naver.com>
2025-07-07 16:54:16 +0900

    모드 체리픽

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          Learn React mode
           Learn React 코드 수정
+          모드 체리픽
         </a>
       </header>
     </div>
