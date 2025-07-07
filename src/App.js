fa5a34f7bca2a0c68b8984c7603760715c3b10bb
wanter9091 <dhqudwo2976@naver.com>
2025-07-07 17:01:04 +0900

    값 수정

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 값 수정
         </a>
       </header>
     </div>
